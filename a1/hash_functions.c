9a21e6d5a2e4b0643c9dbbaf80df713cb8d28f84
Munir Aatina <muniraat@teach.cs.toronto.edu>
2017-02-06 15:51:26 -0500

    updated with p2

--- a1/hash_functions.c
+++ a1/hash_functions.c
@@ -4,9 +4,23 @@
 
 
 void hash(char *hash_val, long block_size) {
-
+int i;
+		for(i = 0; i < block_size; i++){
+			hash_val[i] = '\0';
+			} 
+char store_char;
+int j = 0;
+		while(scanf("%c", &store_char) != EOF ){
+				if(j < block_size){ //first wrap around
+						hash_val[j]= (hash_val[j])^store_char; 
+						j++;
+				} else{	// j == block_size
+						j = 0;			
+						hash_val[j] = (hash_val[j])^store_char;
+						j++;
+				 }
+			}
 }
-
 
 int check_hash(const char *hash1, const char *hash2, long block_size) {
 
