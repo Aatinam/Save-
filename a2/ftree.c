c002fc445cafd8b1cdd8ec92596e344a9b05a78d
Munir Aatina <muniraat@teach.cs.toronto.edu>
2017-02-22 01:19:48 -0500

    something

--- a2/ftree.c
+++ a2/ftree.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 // Add your system includes here.
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include <errno.h> 
 
 #include "ftree.h"
 #include "hash.h"
@@ -10,23 +18,30 @@ struct TreeNode *fill_node(const char* path){
 
 struct stat *buff;
 	if(lstat(path, buff) != 0){ //error
-		printf("%s\n", sterror(errno));
+		printf("%s\n", strerror(errno));
 	}else{
 		
-		struct TreeNode *new_node = malloc(sizeof(TreeNode));
+		struct TreeNode *new_node = malloc(sizeof(struct TreeNode));
 		/*set name */
-		char *extract_name = strrchar(path, '/'); //last occurrence of '/'
-		int num_char_to_cpy = path[strlen(path) -1] - extract_name + 1; //calculating length of name 
-		strncpy((new_node -> fname), (extract_name + 1), num_char_to_cpy);	
+		char *extract_name; 
+		extract_name = strrchr(path, '/'); //last occurrence of '/'
+		int path_len = strlen(path);
+		int num_char_to_cpy = &(path[path_len - 1]) - extract_name + 1; //calculating length of name 
+		strncpy((new_node->fname), (extract_name + 1), num_char_to_cpy);	
 	
 		/*set permissions*/
-		new_node -> persissions = ((buff -> st_mode) & 0777); 	
+		new_node -> permissions = ((buff -> st_mode) & 0777); 	
 		
 		/*	set contents if directory, set hash if file or link*/
-		if(S_ISREG(buff -> st_mode) || S_ISLNK(buff -> st_mode){ //file or link 
+		if(S_ISREG(buff -> st_mode) || S_ISLNK(buff -> st_mode)){ //file or link 
 			new_node -> contents = NULL;
-			new_node -> hash = hash(path); //strcpy or is this okay??
-		}if(S_ISDIR(buff -> st_mode) {
+			FILE *f;			
+			if((f= fopen(path, "r")) != NULL){
+			new_node -> hash = hash(f);
+			}else{
+				printf("Incorrect File Path");
+				}	 //strcpy or is this okay??
+		}if(S_ISDIR(buff -> st_mode)) {
 			new_node -> hash = NULL;
 			struct dirent *dp;
 			DIR *dirp = opendir(path);
@@ -35,8 +50,10 @@ struct stat *buff;
 					exit(1);
 				} else {
 					dp = readdir(dirp); //if path is not null read first element in dir
-	 				if(dp != NULL && (dp -> d_name)[0]) != '.' ){
-	 						int required_length = strlen(path) + strlen(dp -> d_name) + 5;
+	 				if(dp != NULL && ((dp -> d_name)[0] != '.')){
+	 	               int path_len = strlen(path);
+	 	               int name_len = strlen(dp -> d_name);
+	 						int required_length = path_len + name_len + 5;
 	 						char new_path[required_length];
 	 						strcat(new_path, path);
 	 						strcat(new_path, '/'); //do i need escape char
