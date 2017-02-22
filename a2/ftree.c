27884eeac40fa33b92edd4a8a088b4aa672684cd
Munir Aatina <muniraat@teach.cs.toronto.edu>
2017-02-22 00:08:59 -0500

    recurssion try 1

--- a2/ftree.c
+++ a2/ftree.c
@@ -13,21 +13,21 @@ struct stat *buff;
 		printf("%s\n", sterror(errno));
 	}else{
 		
-		struct TreeNode new_node;
+		struct TreeNode *new_node = malloc(sizeof(TreeNode));
 		/*set name */
 		char *extract_name = strrchar(path, '/'); //last occurrence of '/'
-		int num_char_to_cpy = path[strlen(path) -1] - extract_name; //calculating length of name 
-		strncpy(new_node.fname, (extract_name + 1), num_char_to_cpy);	
+		int num_char_to_cpy = path[strlen(path) -1] - extract_name + 1; //calculating length of name 
+		strncpy((new_node -> fname), (extract_name + 1), num_char_to_cpy);	
 	
 		/*set permissions*/
-		new_node.persissions = ((buff -> st_mode) & 0777); 	
+		new_node -> persissions = ((buff -> st_mode) & 0777); 	
 		
 		/*	set contents if directory, set hash if file or link*/
 		if(S_ISREG(buff -> st_mode) || S_ISLNK(buff -> st_mode){ //file or link 
-			new_node.contents = NULL;
-			new_node.hash = hash(path); //strcpy or is this okay??
+			new_node -> contents = NULL;
+			new_node -> hash = hash(path); //strcpy or is this okay??
 		}if(S_ISDIR(buff -> st_mode) {
-			new_node.hash = NULL;
+			new_node -> hash = NULL;
 			struct dirent *dp;
 			DIR *dirp = opendir(path);
 				if(dirp == NULL) {
@@ -41,13 +41,19 @@ struct stat *buff;
 	 						strcat(new_path, path);
 	 						strcat(new_path, '/'); //do i need escape char
 	 						strcat(new_path, (dp-> d_name));
-	 						new_node.contents = fill_node(new_path);
-					  }
+	 						new_node -> contents = fill_node(new_path);
+					  }dp = readdir(dirp);
+					  if(dp != NULL && (dp -> d_name)[0]) != '.' ){
+	 						int required_length = strlen(path) + strlen(dp -> d_name) + 5;
+	 						char new_path[required_length];
+	 						strcat(new_path, path);
+	 						strcat(new_path, '/'); //do i need escape char
+	 						strcat(new_path, (dp-> d_name));
+	 						(new_node -> contents) -> next = fill_node(new_path);
 				 }
 			}
 			/*set next*/
-		   readdir(path) //read dir on same path level as first func call, still need to do opendir and stuff 
-		   if
+		   new_node -> next = NULL;
 	}
 }
 
