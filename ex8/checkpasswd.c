2be47b434a7b2897c3524d041b2f762aa5cfe774
Munir Aatina <muniraat@teach.cs.toronto.edu>
2017-03-10 16:53:57 -0500

    completed

--- ex8/checkpasswd.c
+++ ex8/checkpasswd.c
@@ -6,12 +6,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/* Read a user id and password from standard input, 
+/* Read a user id and password from standard input,
    - create a new process to run the validate program
-   - send 'validate' the user id and password on a pipe, 
-   - print a message 
-        "Password verified" if the user id and password matched, 
-        "Invalid password", or 
+   - send 'validate' the user id and password on a pipe,
+   - print a message
+        "Password verified" if the user id and password matched,
+        "Invalid password", or
         "No such user"
      depending on the return value of 'validate'.
 */
@@ -34,7 +34,58 @@ int main(void) {
     printf("Password:\n");
     scanf("%s", password);
 
-	
+    int pipefd[2];
+    int ret_fork;
+    int ret_child;
+    int status;
+    if(pipe(pipefd) == -1){
+      perror("Pipe exited with error");
+    }else{
+      ret_fork = fork();
+    }
 
+    if(ret_fork > 0){//parent
+      close(pipefd[0]); //close read we wn't be reading from pipe
+      if (write(pipefd[1], userid, 10) == -1){
+        perror("Error while writing userid");
+      }
+      if(write(pipefd[1], password, 10) == -1){
+        perror("Error while writing password");
+      }
+      /*finished writing close pipe so reading can begin */
+      close(pipefd[1]);
+      /*wait for child to finish */
+      if (wait(&status) != -1){
+        if (WIFEXITED(status)) {
+          ret_child = WEXITSTATUS(status);
+          /*to print to stdout*/
+          if(ret_child == 0){
+            printf("%s", VERIFIED);
+          } if(ret_child == 1){
+            printf("%s", OTHER);
+          } if(ret_child == 2){
+            printf("%s", BAD_PASSWORD );
+          } if(ret_child == 3){
+            printf("%s", BAD_USER);
+          }
+        } else {
+          printf("Child exited abnormally\n");
+        }
+      }
+    }
+
+    if(ret_fork == 0){//child
+      close(pipefd[1]); //close write child will not write to pipe
+
+      /*reset stdin so it reads from pipe */
+      if((dup2(pipefd[0], fileno(stdin))) == -1){
+        perror("dup2");
+        exit(-1);
+      }
+
+      /*since we reset stdin to read from pipe we don't need pipefd[0]*/
+      close(pipefd[0]);
+        execl("validate", "validate", (char *) 0);
+  }
     return 0;
 }
